a6eb1e5fe07e43129e63ce5a26f70cb9185f69a2
elijahmm17 <elijahmm17@gmail.com>
2019-05-14 02:00:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,11 @@
 # Write your code here.
 
+def line(array)
+  if array.size = 0 
+    puts "The line is currently empty."
+  else 
+    array.each do |customer|
+      puts customer
+    end 
+  end 
+end 
