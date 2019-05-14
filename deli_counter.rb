66641ff8d20e9d6ba28ec0ec4370b3b7812b5e00
elijahmm17 <elijahmm17@gmail.com>
2019-05-14 02:06:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,8 @@
 # Write your code here.
 
 def line(array)
-  if array.size == 0 
-    puts "there is nobody in line"
-  else 
-    print "line is currently: " 
-    array.each_with_index do |customer, index|
-      puts "#{index + 1 }. #{customer} "
-    end 
-  end 
+  if array.size > 0 
+    
 end 
+
+katz_deli = []
